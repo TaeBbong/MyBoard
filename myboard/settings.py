c197079a5a8305ffa9f0891d05ebc671410e076c
TaeBbong <mok05289@korea.ac.kr>
2021-03-28 14:38:59 +0900

    Ready to Deploy

--- myboard/settings.py
+++ myboard/settings.py
@@ -29,8 +29,7 @@ SECRET_KEY = os.environ['secret_key']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1:8000']
 
 # Application definition
 
